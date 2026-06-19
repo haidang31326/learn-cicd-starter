1fa87f31add3ce8ae6f5e92264ed13d701f31eaa
haidang31326 <mygithubdane@gmail.com>
2026-06-19 12:57:25 +0000

    add lib time and comment for pass error

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -93,7 +94,7 @@ func main() {
 		Handler:           router,
 		ReadHeaderTimeout: 5 * time.Second,
 	}
-
+	// #nosec G706
 	log.Printf("Serving on port: %q\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
