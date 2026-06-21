d06842488a971145d59d109ab95c9710a9004686
haidang31326 <mygithubdane@gmail.com>
2026-06-21 10:00:51 +0000

    fix: call goose via absolute go bin path

--- scripts/migrateup.sh
+++ scripts/migrateup.sh
@@ -2,4 +2,7 @@
 if [ -f .env ]; then
   source .env
 fi
-goose -dir sql/schema turso "$DATABASE_URL" up
+
+# Lấy đường dẫn chuẩn xác của thư mục Go bin và gọi trực tiếp goose
+GO_BIN_PATH=$(go env GOPATH)/bin
+$GO_BIN_PATH/goose -dir sql/schema turso "$DATABASE_URL" up
