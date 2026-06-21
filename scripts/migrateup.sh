d07230881a46d3b1a1d32197f92f936a62123e5e
haidang31326 <mygithubdane@gmail.com>
2026-06-21 10:13:48 +0000

    fix: clean script and ensure goose is installed in workflow

--- scripts/migrateup.sh
+++ scripts/migrateup.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-if [ -f .env ]; then
-  source .env
-fi
-
-# Lấy đường dẫn chuẩn xác của thư mục Go bin và gọi trực tiếp goose
-GO_BIN_PATH=$(go env GOPATH)/bin
-$GO_BIN_PATH/goose -dir sql/schema turso "$DATABASE_URL" up
+goose -dir sql/schema turso "$DATABASE_URL" up
